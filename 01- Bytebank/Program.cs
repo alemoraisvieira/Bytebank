f9c4641b7ff910ff07552e2fe546c9a9c74f8e62
alemoraisvieira <62805016+alemoraisvieira@users.noreply.github.com>
2020-04-21 16:27:41 -0300

    Adicionar arquivos de projeto.

---
+++ 01- Bytebank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01__Bytebank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string titular = "Gabriela";
+            int numeroAgencia = 1146;
+            int numeroConta = 24897;
+            double saldo = 100;
+
+            string titular2 = "Bruno";
+            int numeroAgencia2 = 1146;
+            int numeroConta2 = 14898;
+            double saldo2 = 1000;
+
+        }
+    }
+}
